72d7a6dea6da0ac9717b2be530c11b78092ac713
Guilherme Moretti <gmoretti@thoughtworks.com>
2018-09-19 00:02:48 -0300

    refactor on foe ia

--- src/foe.js
+++ src/foe.js
@@ -1,10 +1,7 @@
 import MoveActions from './move_actions';
 
-export default class Foe {
-  constructor(game, pos, attrs) {
-    this.game = game;
-    this.pos = pos;
-    this.color = '#3E3';
+class FoeBrain {
+  constructor() {
     this.moves = [
       MoveActions.moveUp,
       MoveActions.moveDown,
@@ -12,26 +9,41 @@ export default class Foe {
       MoveActions.moveLeft,
       MoveActions.stationate
     ];
+  }
+
+  nextAction(context) {
+    const possibleMoves = [];
+    for (let move of this.moves) {
+      const moveInstance = new move();
+      if (moveInstance.cost <= context.me.actionPoints) { 
+        possibleMoves.push(move);
+      }
+    }
+
+    return possibleMoves[Math.floor(Math.random()*possibleMoves.length)];
+  }
+}
+
+export default class Foe {
+  constructor(game, pos, attrs) {
+    this.game = game;
+    this.pos = pos;
+    this.color = '#3E3';
     this.hp = attrs.hp || 10;
     this.speed = attrs.speed || 50;
     this.name = attrs.name || 'Unknow';
     this.actionPoints = 0;
     this.action = new MoveActions.stationate();
+
+    this.brain = new FoeBrain();
   }
 
   tick() {
     this.actionPoints += this.speed;
     if (this.actionPoints > 100) { this.actionPoints = 100; }
 
-    const possibleMoves = [];
-    for (let move of this.moves) {
-      const moveInstance = new move();
-      if (moveInstance.cost <= this.actionPoints) { possibleMoves.push(move); }
-    }
-
-    const randAction = possibleMoves[Math.floor(Math.random()*possibleMoves.length)];
-
-    this.action = new randAction();
+    const nextAction = this.brain.nextAction({me: this});
+    this.action = new nextAction();
     this.actionPoints -= this.action.cost;
   }
 
